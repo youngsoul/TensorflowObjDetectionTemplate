5d57d4251ad0033014c39188e4ca119e33709ef4
patrickryan <theyoungsoul@gmail.com>
2021-02-03 18:22:22 -0500

    attempting to pin all of the libraries in setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,13 @@ setup(
     author_email='pat_ryan_99@yahoo.com',
     description='',
     install_requires = [
-        'tensorflow',
-        'opencv-python',
-        'opencv-contrib-python',
         'numpy==1.19.5',
-        'pandas',
-        'scikit-learn'
+        'pycocotools==2.0.1'
+        'tensorflow==2.4.1',
+        'opencv-python==4.5.1.48',
+        'opencv-contrib-python',
+        'pandas==1.2.1',
+        'scikit-learn==0.24.1'
     ]
 )
 
