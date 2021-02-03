a643aa6b8a5e5c9f2010e2303fe051aa51032cca
patrickryan <theyoungsoul@gmail.com>
2021-02-03 14:01:50 -0500

    print the training command

--- scripts/model_train.py
+++ scripts/model_train.py
@@ -17,12 +17,15 @@ if __name__ == '__main__':
     research_path = f"{abs_models_repo_path}/models/research"
     object_detection_path = f"{research_path}/object_detection"
 
-    os.system(f"python {object_detection_path}/model_main_tf2.py \
-    --pipeline_config_path {mc.CHECKPOINT_PATH}/{mc.base_pipeline_file} \
-    --model_dir={mc.CHECKPOINT_PATH} \
-    --alsologtostderr \
-    --num_train_steps={mc.num_training_steps}"
-    )
+    cmd = f"python {object_detection_path}/model_main_tf2.py \
+        --pipeline_config_path {mc.CHECKPOINT_PATH}/{mc.base_pipeline_file} \
+        --model_dir={mc.CHECKPOINT_PATH} \
+        --alsologtostderr \
+        --num_train_steps={mc.num_training_steps}"
+
+    print(f"Executing: \n{cmd}")
+
+    os.system(cmd)
 
 
 
